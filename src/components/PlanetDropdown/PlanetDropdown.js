4f1a49e730ab5bf04d9fa3d0e03b61ec0f66fe69
unknown <sarmajahnabi8@gmail.com>
2023-07-29 21:00:41 +0530

    Removed autocomplete dropdown functionality

--- src/components/PlanetDropdown/PlanetDropdown.js
+++ src/components/PlanetDropdown/PlanetDropdown.js
@@ -1,65 +1,71 @@
-import { useState } from "react";
 import styles from "../PlanetDropdown/planetDropdown.module.css";
+import Button from "../common/Button/Button";
+import classes from "../common/Button/button.module.css";
 
-const handleOptionSelection = (
-  option,
-  index,
-  setSelectedOption,
-  handlePlanetSelection,
-  handlePlanetToggle
-) => {
-  setSelectedOption(option);
-  handlePlanetSelection(index, option);
-  handlePlanetToggle(index);
-};
+// const handleOptionSelection = (
+//   option,
+//   index,
+//   setSelectedOption,
+//   handlePlanetSelection,
+//   handlePlanetToggle
+// ) => {
+//   setSelectedOption(option);
+//   handlePlanetSelection(index, option);
+//   handlePlanetToggle(index);
+// };
 
-// Function to filter planet options based on user input
-const filterPlanetOptions = (planetOptions, userInput) =>
-  planetOptions.filter((option) =>
-    option.name.toLowerCase().includes(userInput.toLowerCase())
+// // Function to filter planet options based on user input
+// const filterPlanetOptions = (planetOptions, userInput) =>
+//   planetOptions.filter((option) =>
+//     option.name.toLowerCase().includes(userInput.toLowerCase())
+//   );
+
+//Function to filter planet options
+const filterPlanetOptions = (planetOptions, planetDropdowns, index) => {
+  return planetOptions.filter((option) =>
+    planetDropdowns.every((d, i) => i === index || d.selected !== option.name)
   );
+};
 
 export default function PlanetDropdown({
   dropdown,
   index,
   planetOptions,
   planetDropdowns,
-  setPlanetDropdowns,
   handlePlanetToggle,
   handlePlanetSelection,
-  userInput,
-  handleInput,
 }) {
-  const [selectedOption, setSelectedOption] = useState(null);
+  // const [selectedOption, setSelectedOption] = useState(null);
 
   return (
     <>
       <p>Destination {`${index + 1}`}</p>
-      <input
+      {/* <input
         type="text"
         value={selectedOption ? selectedOption.name : userInput}
         onChange={(e) => handleInput(e.target.value)}
         onClick={() => handlePlanetToggle(index)}
         placeholder="Start typing to search..."
-      />
-      {planetDropdowns[index].isOpen && userInput && (
+      /> */}
+      <Button
+        onClick={() => handlePlanetToggle(index)}
+        className={classes.dropdown_button}
+      >
+        {dropdown.selected ? dropdown.selected.name : "Select an option"}
+      </Button>
+
+      {dropdown.isOpen && (
         <ul className={styles.planets_dropdown_list}>
-          {filterPlanetOptions(planetOptions, userInput).map((option) => (
-            <li
-              key={`dropdown-${index}-${option.name}`}
-              onClick={() =>
-                handleOptionSelection(
-                  option,
-                  index,
-                  setSelectedOption,
-                  handlePlanetSelection,
-                  handlePlanetToggle
-                )
-              }
-            >
-              {option.name}
-            </li>
-          ))}
+          {filterPlanetOptions(planetOptions, planetDropdowns, index).map(
+            (option) => (
+              <li
+                key={`dropdown-${index}-${option.name}`}
+                onClick={() => handlePlanetSelection(index, option)}
+              >
+                {option.name}
+              </li>
+            )
+          )}
         </ul>
       )}
     </>
