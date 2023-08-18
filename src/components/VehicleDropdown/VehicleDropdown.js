2f97df96f2202b2faaf73ae97fffef28c5a99c5f
unknown <sarmajahnabi8@gmail.com>
2023-08-18 21:28:03 +0530

    changed disabled behaviour of vehicles

--- src/components/VehicleDropdown/VehicleDropdown.js
+++ src/components/VehicleDropdown/VehicleDropdown.js
@@ -6,39 +6,41 @@ export default function VehicleDropdown({
   handleVehicleSelection,
   vehicleOptions,
 }) {
-
   console.log("vehicleOptions", vehicleOptions);
   return (
     <>
       {dropdown.isOpen && (
         <ul className={styles.vehicles_dropdown_list}>
-          {vehicleOptions
-            .filter(
-              (option) =>
-                option.total > 0 &&
-                option.maxDistance >= dropdown.selectedPlanet.distance
-            )
-            .map((option, optionIndex) => {
-              const remainingCount = option.total - option.used;
-              return (
-                <li key={`option-${index}-${optionIndex}`}>
-                  <label>
-                    <input
-                      type="radio"
-                      name={`vehicle-${index}`}
-                      value={option.name}
-                      checked={option.name === dropdown.currentSelectedVehicle}
-                      onChange={() =>
-                        handleVehicleSelection(index, option.name)
-                      }
-                      disabled={remainingCount === 0}
-                    />
-                    <span>{option.name}</span>
-                    <span>({remainingCount})</span>
-                  </label>
-                </li>
-              );
-            })}
+          {vehicleOptions.map((option, optionIndex) => {
+            const remainingCount = option.total - option.used;
+            const isUnclickable = remainingCount === 0;
+            const isDisabled =
+              option.maxDistance < dropdown.selectedPlanet.distance;
+            const labelClasses = [];
+
+            if (isDisabled) {
+              labelClasses.push(styles["disabled-label"]);
+            }
+            if (isUnclickable) {
+              labelClasses.push(styles["unclickable-label"]);
+            }
+            return (
+              <li key={`option-${index}-${optionIndex}`}>
+                <label className={labelClasses.join(" ")}>
+                  <input
+                    type="radio"
+                    name={`vehicle-${index}`}
+                    value={option.name}
+                    checked={option.name === dropdown.currentSelectedVehicle}
+                    onChange={() => handleVehicleSelection(index, option.name)}
+                    disabled={isDisabled || isUnclickable}
+                  />
+                  <span>{option.name}</span>
+                  <span>({remainingCount})</span>
+                </label>
+              </li>
+            );
+          })}
         </ul>
       )}
     </>
