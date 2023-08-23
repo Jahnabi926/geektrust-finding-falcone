88c30a85bdfb352cb762cc27e388cd8462cbb38a
unknown <sarmajahnabi8@gmail.com>
2023-08-23 21:41:53 +0530

    added test cases - 2

--- src/helpers/__tests__/homeUtils.test.js
+++ src/helpers/__tests__/homeUtils.test.js
@@ -1,48 +1,9 @@
 import {
-  updatePlanetDropdowns,
   updateAssociatedVehicleDropdown,
-  handlePlanetToggle,
   calculateTimeTaken,
 } from "../home.utils";
 
 describe("Utility Functions", () => {
-  const planetOptions = [
-    { name: "Donlon", distance: 100 },
-    { name: "Enchai", distance: 200 },
-    { name: "Sapir", distance: 300 },
-    { name: "Pingasor", distance: 400 },
-  ];
-
-  const initialDropdowns = [
-    { selectedPlanet: null, isOpen: false },
-    { selectedPlanet: null, isOpen: false },
-    { selectedPlanet: null, isOpen: false },
-    { selectedPlanet: null, isOpen: false },
-  ];
-
-  test("updates planet dropdowns correctly", () => {
-    const value = { name: "Enchai" };
-    const updatedDropdowns = updatePlanetDropdowns(
-      initialDropdowns,
-      1,
-      value,
-      planetOptions
-    );
-
-    // Asserts the updated dropdowns and options
-    expect(updatedDropdowns[1].selectedPlanet).toEqual(value);
-    expect(updatedDropdowns[1].isOpen).toEqual(false);
-
-    // Assert the selectedByDropDown property of planet options
-    expect(
-      updatedDropdowns[1].filteredPlanetOptions[1].selectedByDropDown
-    ).toEqual("d2");
-
-    expect(
-      updatedDropdowns[0].filteredPlanetOptions[1].selectedByDropDown
-    ).toBeUndefined();
-  });
-
   test("updates associated vehicle dropdown correctly", () => {
     const selectedPlanet = { name: "Donlon" };
     const associatedDropdown = {
@@ -58,28 +19,9 @@ describe("Utility Functions", () => {
       vehicleDropdowns
     );
 
-    // Assert the updated selected planet and isOpen property
+    // Asserts the updated selected planet and isOpen property
     expect(updatedDropdowns[0].selectedPlanet).toEqual(selectedPlanet);
     expect(updatedDropdowns[0].isOpen).toEqual(true);
-  });
-
-  test("handles planet toggle correctly", () => {
-    const initialDropdowns = [
-      { isOpen: false },
-      { isOpen: false },
-      { isOpen: false },
-      { isOpen: false },
-    ];
-
-    const updatedDropdowns = handlePlanetToggle(1, initialDropdowns);
-
-    // Assert that the toggle for index 1 is now true
-    expect(updatedDropdowns[1].isOpen).toEqual(true);
-
-    // Assert that the toggle for other indices remains unchanged
-    expect(updatedDropdowns[0].isOpen).toEqual(false);
-    expect(updatedDropdowns[2].isOpen).toEqual(false);
-    expect(updatedDropdowns[3].isOpen).toEqual(false);
   });
 
   test("calculates time taken correctly", () => {
@@ -88,7 +30,7 @@ describe("Utility Functions", () => {
 
     const timeTaken = calculateTimeTaken(planet, vehicle);
 
-    // Assert the calculated time taken
+    // Asserts the calculated time taken
     expect(timeTaken).toEqual(100); // 200 / 2 = 100
   });
 
@@ -98,7 +40,7 @@ describe("Utility Functions", () => {
 
     const timeTaken = calculateTimeTaken(planet, vehicle);
 
-    // Assert the calculated time taken as 0 when vehicle is missing
+    // Asserts the calculated time taken as 0 when vehicle is missing
     expect(timeTaken).toEqual(0);
 
     const planetMissing = null;
@@ -106,7 +48,7 @@ describe("Utility Functions", () => {
 
     const timeTakenMissing = calculateTimeTaken(planetMissing, vehicleMissing);
 
-    // Assert the calculated time taken as 0 when planet is missing
+    // Asserts the calculated time taken as 0 when planet is missing
     expect(timeTakenMissing).toEqual(0);
   });
 });
--- src/pages/Home/__tests__/Home.test.js
+++ src/pages/Home/__tests__/Home.test.js
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { MemoryRouter, Routes, Route } from "react-router-dom";
 import Home from "../Home";
 
@@ -32,50 +32,6 @@ describe("Home Component", () => {
     // Makes sure the "Find Falcone" button is initially disabled
     const findButton = screen.getByText("Find Falcone!");
     expect(findButton).toBeDisabled();
-  });
-
-  test("enables Find Falcone button when all selections are made", () => {
-    render(
-      <MemoryRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </MemoryRouter>
-    );
-
-    const planetDropdownCount = 4;
-
-    // Simulates selecting planets
-    for (let index = 0; index < planetDropdownCount; index++) {
-      const planetDropdown = screen.getByRole("button", {
-        name: /Select an option/i,
-      });
-
-      fireEvent.click(planetDropdown); // Opens the planet dropdown
-
-      // Selects planet option based on index
-      const planetOption = screen.getByText(/^(Donlon|Enchai|Sapir|Pingasor)$/);
-      fireEvent.click(planetOption);
-    }
-
-    // Simulates selecting vehicles
-    const vehicleRadioButtons = screen.getAllByRole("radio");
-    const vehicleNames = [
-      "Space pod",
-      "Space rocket",
-      "Space shuttle",
-      "Space ship",
-    ];
-
-    vehicleRadioButtons.forEach((radioButton, index) => {
-      fireEvent.click(radioButton);
-      const vehicleOption = screen.getByText(vehicleNames[index]);
-      fireEvent.click(vehicleOption);
-    });
-
-    // Now the "Find Falcone" button should be enabled
-    const findButton = screen.getByText("Find Falcone!");
-    expect(findButton).not.toBeDisabled();
   });
 
   test("matches snapshot", () => {
